177eb9ff7b04eb2c780f7b6f94706ceaf49b16af
marioguddry <mario99cocolego@gmail.com>
2016-05-18 11:42:58 -0500

    jo

---
+++ prueba.php
@@ -0,0 +1,14 @@
+<?php
+	$x = rand(1,10);
+	
+		echo "hola";
+		echo "hello";
+		echo "hallo";
+		echo "marhabaan";
+		echo "Hyālō";
+		echo "ahoj";
+		echo "salut";
+		echo "helo";
+		echo "cyao";
+		echo "hola";
+?>
